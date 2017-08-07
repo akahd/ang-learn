5dad0f5a7ddb3227a1035fb7c53c5dc08935b3eb
hilarydeng <hilarydeng@hilarydengs-MacBook-Pro.local>
2017-08-07 16:54:52 -0400

    assignment1

--- app.js
+++ app.js
@@ -6,23 +6,41 @@
   // this module has been bound to the whole html -- bound the ng-app to the most outer tag
   angular.module('myFirstApp', [])
   // inside the div tag is controlled by the controller
-  .controller('myFirstController', function($scope){
-    //$scope: reserved for angularjs
-    $scope.name = "akahd";
-    $scope.totalValue = 0;
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope', '$filter'];
+  function LunchCheckController($scope) {
+    $scope.userinput = "";
+    $scope.message = "";
 
-    $scope.count = function(){
-      var totalNameValue = calculate($scope.name);
-      $scope.totalValue = totalNameValue;
-    };
-
-    function calculate(string) {
-      var sum = 0;
-      for (var i = 0; i < string.length; i++) {
-        sum += string.charCodeAt(i);
+    $scope.respond = function (){
+      var response = "";
+      var num = countFood($scope.userinput);
+      console.log(num);
+      if (num > 3) {
+        response = "Too Much";
+      } else if (num == -1){
+        response = "Please enter data first";
+      } else {
+        response = "Enjoy!!";
       }
-      return sum;
-    };
+      $scope.userinput = "";
+      $scope.message = response;
+    }
 
-  });
+    function countFood(string) {
+      string.trim();
+      if (string.length === 0) {
+        return -1;
+      }
+      console.log("|" + string + "|");
+      var re = /\s*,\s*/;
+      var arr = string.split(re).map( function(item) {
+        console.log("item:" + item + ";");
+        if (item.match(/\S+/g)) {
+          return item;
+        }
+      });
+      return arr.length;
+    }
+  };
 })();
